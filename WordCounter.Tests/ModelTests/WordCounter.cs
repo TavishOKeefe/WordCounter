faef4efd0857b72c2605c6e5ec4030fe88cdc845
Padma Patil <padma@email.com>
2018-11-16 10:30:04 -0800

    try to run first test and fail many times.

--- WordCounter.Tests/ModelTests/WordCounter.cs
+++ WordCounter.Tests/ModelTests/WordCounter.cs
@@ -1,3 +1,22 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System;
+using WordCounter;
+
+namespace WordCounter.Tests
+{
+  [TestClass]
+  public class WordCounterTests
+  {
+    [TestMethod]
+    public void LetterCompare_CompareFirstLetterToFistLetterOfFirstWord_True()
+    {
+      List<string> sentence = new List<string>{"and", "I", "ran"};
+      WordCounter compareFirst = new WordCounter("and", sentence);
+      string firstWord = compareFirst.LetterCompare();
+      Assert.AreEqual(true, firstWord);
+    }
+
+
+  }
+}
--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -3,22 +3,23 @@ using System;
 
 namespace WordCounter
 {
-  class WordCounter
+  public class WordCounter
   {
     private string _word;
     private List<string> _sentence = new List<string>{};
 
-    public inputedWord (string word)
+    public void inputedWord (string word, List<string> sentence)
     {
       _word = word;
-    }
-
-    public inputedSentence (List<string> sentence)
-    {
       _sentence = sentence;
     }
 
-    
+    public bool LetterCompare ()
+    {
+      return true;
+    }
+
+
 
   }
 }
